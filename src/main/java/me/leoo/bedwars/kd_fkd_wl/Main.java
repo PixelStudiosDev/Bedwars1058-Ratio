451bb31610667b56aa6cc78397e1424a52450594
leo18bernese <leo18bermese@gmail.com>
2023-05-24 18:21:23 +0200

    Initial commit

---
+++ src/main/java/me/leoo/bedwars/kd_fkd_wl/Main.java
@@ -0,0 +1,45 @@
+package me.leoo.bedwars.kd_fkd_wl;
+
+import lombok.Getter;
+import me.leoo.bedwars.kd_fkd_wl.papi.Placeholders;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Getter
+public class Main extends JavaPlugin {
+
+    @Getter
+    private static Main plugin;
+
+    private int bedwarsPlugin;
+
+    public void onEnable() {
+        plugin = this;
+
+        if (Bukkit.getPluginManager().isPluginEnabled("BedWarsProxy")) {
+            bedwarsPlugin = 0;
+        }
+        if (Bukkit.getPluginManager().isPluginEnabled("BedWars1058")) {
+            bedwarsPlugin = 1;
+        }
+        if (bedwarsPlugin != 0 && bedwarsPlugin != 1) {
+            getLogger().severe("Bedwars1058/BedwarsProxy plugin was not found. Disabling...");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") == null) {
+            getLogger().severe("PlaceholderAPI plugin was not found. Disabling...");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        new Placeholders().register();
+
+        getLogger().info(ChatColor.translateAlternateColorCodes('&', "&a" + getDescription().getName() + " plugin by itz_leoo has been successfully enabled."));
+    }
+
+    public void onDisable() {
+        getLogger().info(ChatColor.translateAlternateColorCodes('&', "&c" + getDescription().getName() + " plugin by itz_leoo has been successfully disabled."));
+    }
+}
---
+++ src/main/java/me/leoo/bedwars/kd_fkd_wl/papi/Placeholders.java
@@ -0,0 +1,57 @@
+package me.leoo.bedwars.kd_fkd_wl.papi;
+
+import com.andrei1058.bedwars.BedWars;
+import com.andrei1058.bedwars.proxy.BedWarsProxy;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import me.leoo.bedwars.kd_fkd_wl.Main;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DecimalFormat;
+
+public class Placeholders extends PlaceholderExpansion {
+    @NotNull
+    public String getAuthor() {
+        return "itz_leoo";
+    }
+
+    @NotNull
+    public String getIdentifier() {
+        return "bw1058stats";
+    }
+
+    @NotNull
+    public String getVersion() {
+        return Main.getPlugin().getDescription().getVersion();
+    }
+
+    @Override
+    public boolean persist() {
+        return true;
+    }
+
+    public String onRequest(OfflinePlayer player, String params) {
+        DecimalFormat decimalFormat = new DecimalFormat("#####.##");
+
+        if (params.equalsIgnoreCase("kdr")) {
+            if (Main.getPlugin().getBedwarsPlugin() == 0) {
+                return decimalFormat.format(BedWarsProxy.getStatsCache().getPlayerKills(player.getUniqueId()) / BedWarsProxy.getStatsCache().getPlayerDeaths(player.getUniqueId()));
+            }
+            return decimalFormat.format(BedWars.getStatsManager().get(player.getUniqueId()).getKills() / BedWars.getStatsManager().get(player.getUniqueId()).getDeaths());
+        }
+        if (params.equalsIgnoreCase("fkdr")) {
+            if (Main.getPlugin().getBedwarsPlugin() == 0) {
+                return decimalFormat.format(BedWarsProxy.getStatsCache().getPlayerFinalKills(player.getUniqueId()) / BedWarsProxy.getStatsCache().getPlayerFinalDeaths(player.getUniqueId()));
+            }
+            return decimalFormat.format(BedWars.getStatsManager().get(player.getUniqueId()).getFinalKills() / BedWars.getStatsManager().get(player.getUniqueId()).getFinalDeaths());
+        }
+        if (params.equalsIgnoreCase("wlr")) {
+            if (Main.getPlugin().getBedwarsPlugin() == 0) {
+                return decimalFormat.format(BedWarsProxy.getStatsCache().getPlayerWins(player.getUniqueId()) / BedWarsProxy.getStatsCache().getPlayerLoses(player.getUniqueId()));
+            }
+            return decimalFormat.format(BedWars.getStatsManager().get(player.getUniqueId()).getWins() / BedWars.getStatsManager().get(player.getUniqueId()).getLosses());
+        }
+
+        return null;
+    }
+}
